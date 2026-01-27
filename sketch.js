187faa9b540ebadd4b18fa3d8ea53cb9fdf2cc89
Your Name <you@example.com>
2026-01-27 12:46:58 -0800

    broadens stroke/change color

--- sketch.js
+++ sketch.js
@@ -1,20 +1,30 @@
+let bgColor;
+
 function setup() {
   createCanvas(400, 400);
+  noStroke();
+  bgColor = color(0);
 }
 
+
 function draw() {
-  background(0);
   beginShape();
-    strokeWeight(2)
+    strokeWeight(4);
     stroke(256, 256, 256);
     vertex(20, 80);
     bezierVertex(430, 360, 234, 167, 130, 304);
       beginContour();
-        stroke(210, 0, 180);
+        stroke(250, 0, 180);
         vertex(40, 50);
         vertex(100, 200);
         vertex(350, 280);
       endContour();
   endShape();
 
+}
+
+function mousePressed() {
+  // Set the background to a new random grayscale value
+  bgColor = random(255), random(255), random(255);
+  background(bgColor); 
 }
