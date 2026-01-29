8a003c3e115f41f12a4e407886131351750ab7b7
Your Name <you@example.com>
2026-01-29 10:44:30 -0800

    final version

--- sketch.js
+++ sketch.js
@@ -1,20 +1,21 @@
-let bgColor;
+
 
 function setup() {
   createCanvas(400, 400);
-  noStroke();
-  bgColor = color(0);
+  colorMode(HSB, 360, 100, 100);
+  background(360, 0 ,0);
+  
+
 }
 
 
 function draw() {
   beginShape();
-    strokeWeight(4);
-    stroke(256, 256, 256);
+    strokeWeight(5);
     vertex(20, 80);
-    bezierVertex(430, 360, 234, 167, 130, 304);
+    bezierVertex(430, 360, 234, 167, 130, 334);
       beginContour();
-        stroke(250, 0, 180);
+        stroke(85, 100, 100);
         vertex(40, 50);
         vertex(100, 200);
         vertex(350, 280);
@@ -23,8 +24,3 @@ function draw() {
 
 }
 
-function mousePressed() {
-  // Set the background to a new random grayscale value
-  bgColor = random(255), random(255), random(255);
-  background(bgColor); 
-}
